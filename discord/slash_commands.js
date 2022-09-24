f7d34cf6abd4cbb5ea50cce510cec1c58b0c6285
Dennis Tsiang <24190844+DennisTsiang@users.noreply.github.com>
2022-09-24 19:55:03 +0100

    Support @mentions in whois slash command

--- discord/slash_commands.js
+++ discord/slash_commands.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, MessageMentions: { GlobalUsersPattern }} = require("discord.js");
 const { whois } = require("./commands.js");
 
 const commandsJSON = [
@@ -14,11 +14,29 @@ const commandsJSON = [
 
 var whois_interaction = async (interaction) => {
     var nick = interaction.options.getString("nick");
+    var mention = await getUsernameFromMention(interaction, nick);
+    if (mention != null) {
+        nick = mention;
+    }
     await whois(nick, interaction.client, async (string) => {
         await interaction.editReply(string);
     });
 };
 
+async function getUsernameFromMention(interaction, mention) {
+    // The id is the first and only match found by the RegEx.
+    const matches = mention.matchAll(GlobalUsersPattern).next().value;
+
+    // If supplied variable was not a mention, matches will be null instead of an array.
+    if (!matches) return null;
+
+    // The first element in the matches array will be the entire mention, not just the ID,
+    // so use index 1.
+    const id = matches[1];
+
+    var user = await interaction.client.users.fetch(id);
+    return user.username;
+}
 
 const commandsTable = new Map([
     ["whois", whois_interaction]
